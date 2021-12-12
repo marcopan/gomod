23e2193f5fe65aa374db3fd8f4d5f9f50359a805
marcopan <370510052@qq.com>
2021-12-12 14:00:48 +0800

    add gin html

--- mod1/gin.go
+++ mod1/gin.go
@@ -7,9 +7,26 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello world!")
-	})
+
+	r.Static("/css", "./templates/css")
+	r.LoadHTMLGlob("templates/*.html")
+
+	group := r.Group("/api")
+	{ // http://localhost:6066/api/hello
+		group.GET("/hell", func(c *gin.Context) {
+			c.String(http.StatusOK, "Hello world!")
+		})
+	}
+	viwGroup := r.Group("/view")
+	{ // http://localhost:6066/view/index
+		viwGroup.GET("/index", func(c *gin.Context) {
+			h := gin.H{
+				"title": "test title",
+				"body":  "body test",
+			}
+			c.HTML(http.StatusOK, "index.html", h)
+		})
+	}
 
 	r.Run(":6066")
 }
